e3fcd733e1c808deed06c8dc59537c8700c76056
ReenRex <governed.rex@gmail.com>
2026-07-17 19:57:45 +0100

    Update francedata.js

--- France/francedata.js
+++ France/francedata.js
@@ -31,12 +31,12 @@ data.Europe.France = {
   "19th Century": [],
   "20th Century": [
     {
-      title: "Normandy 1:25k Maps",
+      title: "Normandy 1:25k Maps (1943)",
       url: "https://cdm16630.contentdm.oclc.org/digital/collection/p16630coll8/search/searchterm/France%201%3A25%2C000",
       notes: "These sheets cannot be downloaded and must be stiched together via taking several zoomed-in screenshots. Although this sounds like a lengthy process, it can take around 10-20 screenshots over 25-50 minutes to capture the land on a sheet. This also depends on how zoomed in (how high quality) you wish for your screenshots to be."
     },
     {
-      title: "France 1:50,000 1950 Map",
+      title: "France 1:50,000 1950 Survey Map",
       url: "https://cartes.gouv.fr/rechercher-une-donnee/dataset/IGNF_SCAN-50-HISTO",
       tutorial: {
         intro: 'Scroll down until you see "Resources and Links"',
@@ -48,7 +48,7 @@ data.Europe.France = {
       }
     },
     {
-      title: "Normandy 1:50k Map Sheets",
+      title: "Normandy 1:50k (1943)",
       url: "https://www.normandywarguide.com/articles/gsgs-4250-map-series",
       notes: [
         {
