829a7771520a4a7e703585d8b1ad47e1d3bf1b97
ReenRex <governed.rex@gmail.com>
2026-07-18 15:18:35 +0100

    Update italydata.js

--- Italy/italydata.js
+++ Italy/italydata.js
@@ -9,9 +9,20 @@ data.Europe.Italy = {
   "18th Century": [],
   "19th Century": [],
   "20th Century": [
+     {
+        title: "Italy 1:25k (1942)",
+        url: "https://archiviocartografico.regione.campania.it/ua/fascrc-imm/?page=1"
+     }
+     
+     
      { 
         title: "Italy 1:25k (1943)",
         url: "https://maps.lib.utexas.edu/maps/ams/italy_25k/"
+     },
+
+     {
+        title: "Italy 1:50k (1941)",
+        url: "https://maps.lib.utexas.edu/maps/ams/italy_50k/"
      }
      
   ]
