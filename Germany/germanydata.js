0cc3033bbe60c4319712109e85cde24c3e00803a
ReenRex <governed.rex@gmail.com>
2026-07-18 23:26:30 +0100

    Update germanydata.js

--- Germany/germanydata.js
+++ Germany/germanydata.js
@@ -13,6 +13,10 @@ data.Europe.Germany = {
       title: "Germany 1:25k (1952)",
       url: "https://digicoll.lib.berkeley.edu/record/112517?ln=en&v=uv#?xywh=1825%2C2444%2C2341%2C1902&cv=2279",
       indexUrl: "https://www.loc.gov/resource/g6080m.gct00113b/?sp=1&st=image&r=-0.266,0.033,1.459,0.878,0"
-    }
+    },
+     
+      {
+        title: "Germany 1:25k (1952) -- Alternative Database",
+         url: "https://contentdm.lib.byu.edu/digital/collection/GermanyMaps/search"
   ]
 };
