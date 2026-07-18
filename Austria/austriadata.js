a66390cf5acef80d765ecd7392d9bb29e0d98fcf
ReenRex <governed.rex@gmail.com>
2026-07-18 11:08:43 +0100

    Update austriadata.js

--- Austria/austriadata.js
+++ Austria/austriadata.js
@@ -30,7 +30,9 @@ data.Europe.Austria = {
       tutorial: {
         intro: "This database requires maps to be requested via email. You can fill in the form as seen in the instruction below.",
         steps: [
-          { caption: "", src: "" }
+          { caption: "", src: "Austria/images/step1.png" },
+          { caption: "", src: "Austria/images/step2.png" },
+          { caption: "Fill out the request form with your details and submit. Depending on scan availability, you can get roughly 10-20 scan free of charge per quarter of the year.", src: "" }
         ]
       }
     }
