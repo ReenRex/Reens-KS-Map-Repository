ee525a0c2be6502da7e3c79b02ace898d3359418
ReenRex <governed.rex@gmail.com>
2026-07-19 00:33:18 +0100

    Update germanydata.js

--- Germany/germanydata.js
+++ Germany/germanydata.js
@@ -12,11 +12,18 @@ data.Europe.Germany = {
       title: "East Germany 1:10k (1970-80s)",
       url: "https://ifl.wissensbank.com/esearcha/browse.tt.html",
       notes: [
-        { body: "Unfortunately, I could not find an index for these sheets, so you will have to do your best to piece these together if you wish to use them." },
+        { body: "Unfortunately, I could not find an index for these sheets, so you will have to do your best to piece these together if you wish to use them. I will, however, continue the search for it." },
         { body: "It should also be noted that included in the 1:10k category are many 1:100k maps. This seems to be a mistake of some sort so be careful when choosing a map. Read the scale before getting too excited." }
       ]
     },
-    {
+       {
+      title: "East Germany 1:25k (1970-80s)",
+      url: "https://ifl.wissensbank.com/esearcha/browse.tt.html",
+      notes: [
+        { body: "Unfortunately, I could not find an index for these sheets, so you will have to do your best to piece these together if you wish to use them. I will, however, continue the search for it." },
+     ]
+       },
+     {
       title: "Germany 1:25k (1952)",
       url: "https://digicoll.lib.berkeley.edu/record/112517?ln=en&v=uv#?xywh=1825%2C2444%2C2341%2C1902&cv=2279",
       indexUrl: "https://www.loc.gov/resource/g6080m.gct00113b/?sp=1&st=image&r=-0.266,0.033,1.459,0.878,0"
