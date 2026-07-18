c972359c61b3ef10fa7228fa2d8b3534158f58ae
ReenRex <governed.rex@gmail.com>
2026-07-18 21:13:23 +0100

    Update germanydata.js

--- Germany/germanydata.js
+++ Germany/germanydata.js
@@ -10,7 +10,7 @@ data.Europe.Germany = {
   "19th Century": [],
   "20th Century": [
        {
-      title: "Germany 1:25k (1943)",
+      title: "Germany 1:25k (1952)",
       url: "https://digicoll.lib.berkeley.edu/record/112517?ln=en&v=uv#?xywh=1825%2C2444%2C2341%2C1902&cv=2279",
       indexUrl: "https://www.loc.gov/resource/g6080m.gct00113b/?sp=1&st=image&r=-0.266,0.033,1.459,0.878,0"
     }
