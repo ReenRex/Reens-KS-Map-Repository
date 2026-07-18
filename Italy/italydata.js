0c1b23da05bc8c035acd039acbb0b3801021f81c
ReenRex <governed.rex@gmail.com>
2026-07-18 15:29:20 +0100

    Update italydata.js

--- Italy/italydata.js
+++ Italy/italydata.js
@@ -4,27 +4,27 @@
    tutorials and notes to an entry.
    ============================================================ */
 data.Europe = data.Europe || {};
-
 data.Europe.Italy = {
   "18th Century": [],
   "19th Century": [],
   "20th Century": [
-    
-     {
-        title: "Italy 1:25k (1942)",
-        url: "https://archiviocartografico.regione.campania.it/ua/fascrc-imm/?page=1"
-     }
-     
-     
-     { 
-        title: "Italy 1:25k (1943)",
-        url: "https://maps.lib.utexas.edu/maps/ams/italy_25k/"
-     },
-
-     {
-        title: "Italy 1:50k (1941)",
-        url: "https://maps.lib.utexas.edu/maps/ams/italy_50k/"
-     },
-     
+    {
+      title: "Italy 1:25k (1942)",
+      url: "https://archiviocartografico.regione.campania.it/ua/fascrc-imm/?page=1",
+      notes: [
+        {
+          body: "These images are borderline usable. Below I have attached a screenshot of their use in TableTop Sim with some Ottovski WW2 Pieces for reference.",
+          image: "Italy/images/tabletop-example.png"
+        }
+      ]
+    },
+    {
+      title: "Italy 1:25k (1943)",
+      url: "https://maps.lib.utexas.edu/maps/ams/italy_25k/"
+    },
+    {
+      title: "Italy 1:50k (1941)",
+      url: "https://maps.lib.utexas.edu/maps/ams/italy_50k/"
+    }
   ]
 };
