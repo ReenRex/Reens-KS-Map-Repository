f6f54fd5fc5d9c46047505c905d286dbb556f91d
ReenRex <governed.rex@gmail.com>
2026-07-18 14:45:38 +0100

    Update italydata.js

--- Italy/italydata.js
+++ Italy/italydata.js
@@ -8,5 +8,11 @@ data.Europe = data.Europe || {};
 data.Europe.Italy = {
   "18th Century": [],
   "19th Century": [],
-  "20th Century": []
+  "20th Century": [
+     { 
+        title: "Italy 1:25k (1943)",
+        url: "https://maps.lib.utexas.edu/maps/ams/italy_25k/"
+     }
+     
+  ]
 };
