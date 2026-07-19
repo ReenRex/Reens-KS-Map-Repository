89f6c98842029ac7f4a2fdf5bb36f9fbd7b738b5
ReenRex <governed.rex@gmail.com>
2026-07-19 11:21:36 +0100

    Update germanydata.js

--- Germany/germanydata.js
+++ Germany/germanydata.js
@@ -26,6 +26,17 @@ data.Europe.Germany = {
       ]
     },
      {
+        title: "German Empire 1:25k (1900s-1940s)",
+        url: "https://ifl.wissensbank.com/esearcha/browse.tt.html",
+         notes: [
+        { body: "Unfortunately, I could not find an index for these sheets, so you will have to do your best to piece these together if you wish to use them. I will, however, continue the search for it." },
+        { body: "It should also be noted that many of the maps in this collection originate from the 1880s-1910s. Despite them being republished in the following decades, the data is likely quite similar to the originals." },
+      ]
+    },
+   
+
+     
+     {
       title: "Germany 1:25k (1952)",
       url: "https://digicoll.lib.berkeley.edu/record/112517?ln=en&v=uv#?xywh=1825%2C2444%2C2341%2C1902&cv=2279",
       indexUrl: "https://www.loc.gov/resource/g6080m.gct00113b/?sp=1&st=image&r=-0.266,0.033,1.459,0.878,0"
