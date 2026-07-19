69f8b90dff3a019d5856fadbf26a81c866d76ac0
ReenRex <governed.rex@gmail.com>
2026-07-19 13:29:00 +0100

    Update germanydata.js

--- Germany/germanydata.js
+++ Germany/germanydata.js
@@ -9,10 +9,7 @@ data.Europe.Germany = {
   "19th Century": [
      {
         title: "Bavaria 1:25k (1880s-1940s)",
-        url: "https://ifl.wissensbank.com/esearcha/browse.tt.html",
-         notes: [
-           { body: "Unfortunately, I could not find an index for these sheets, so you will have to do your best to piece these together if you wish to use them. I will, however, continue the search for it." },
-           ]
+        url: "https://leibniz-ifl.de/forschung/infrastrukturen/bibliothek/kartensammlung/tk-bayern-125-000",
      },
      
   ],
@@ -21,7 +18,7 @@ data.Europe.Germany = {
       title: "East Germany 1:10k (1970-80s)",
       url: "https://ifl.wissensbank.com/esearcha/browse.tt.html",
       notes: [
-        { body: "Unfortunately, I could not find an index for these sheets, so you will have to do your best to piece these together if you wish to use them. I will, however, continue the search for it." },
+    { body: "Unfortunately, the index of this collection doesn't seem to be available, however, you may be able to piece it together using the index found in the 1:25k section. Please read the 1:25k notes beforehand. You essentially just need to match up the map codes when using the 1:25k index to locate them." },
         { body: "It should also be noted that included in the 1:10k category are many 1:100k maps. This seems to be a mistake of some sort so be careful when choosing a map. Read the scale before getting too excited." }
       ]
     },
@@ -49,20 +46,27 @@ data.Europe.Germany = {
       {
       title: "East Germany 1:25k (1970-80s)",
       url: "https://leibniz-ifl.de/forschung/infrastrukturen/bibliothek/kartensammlung/tk-125000-der-ddr",
+ notes: [
+    { body: "It would seem that the indexes included at the bottom don't match the format of the maps in the catalog list. However, the indexes themselves are clickable, meaning you can find maps you need fairly easily."
+   { body: "It would seem that the first twelve or so maps have been removed in the series' catalog list."
+    ]
+         
        },
 
      {
         title: "East Germany 1:50k (1980s)",
-        url: "https://ifl.wissensbank.com/esearcha/browse.tt.html",
+        url: "https://leibniz-ifl.de/forschung/infrastrukturen/bibliothek/kartensammlung",
         notes: [
-           { body: "Unfortunately, I could not find an index for these sheets, so you will have to do your best to piece these together if you wish to use them. I will, however, continue the search for it." },
+           { body: "Unfortunately, the index of this collection doesn't seem to be available, however, you may be able to piece it together using the index found in the 1:25k section. Please read the 1:25k notes beforehand. You essentially just need to match up the map codes when using the 1:25k index to locate them." },
+            { body: "Also, for some reason, the 1:50k GDR maps don't have their own page, so I have to just show you the main page and you find it from there." },
            ]
      },
      {
         title: "East Germany 1:100k (1970s-80s)",
-        url: "https://ifl.wissensbank.com/esearcha/browse.tt.html",
-         notes: [
-           { body: "Unfortunately, I could not find an index for these sheets, so you will have to do your best to piece these together if you wish to use them. I will, however, continue the search for it." },
+        url: "https://leibniz-ifl.de/forschung/infrastrukturen/bibliothek/kartensammlung",
+       notes: [
+           { body: "Unfortunately, the index of this collection doesn't seem to be available, however, you may be able to piece it together using the index found in the 1:25k section. Please read the 1:25k notes beforehand. You essentially just need to match up the map codes when using the 1:25k index to locate them." },
+            { body: "Also, for some reason, the 1:100k GDR maps don't have their own page, so I have to just show you the main page and you find it from there." },
            ]
      },
            
