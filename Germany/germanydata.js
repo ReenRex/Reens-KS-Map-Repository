832e2757d3849f0c9f6d4541a7696f9e299ba1f9
ReenRex <governed.rex@gmail.com>
2026-07-19 12:21:20 +0100

    Update germanydata.js

--- Germany/germanydata.js
+++ Germany/germanydata.js
@@ -49,9 +49,6 @@ data.Europe.Germany = {
       {
       title: "East Germany 1:25k (1970-80s)",
       url: "https://ifl.wissensbank.com/esearcha/browse.tt.html",
-      notes: [
-        { body: "Unfortunately, I could not find an index for these sheets, so you will have to do your best to piece these together if you wish to use them. I will, however, continue the search for it." },
-     ]
        },
 
      {
