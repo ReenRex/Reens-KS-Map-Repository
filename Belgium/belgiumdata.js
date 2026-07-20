edf5469e841622f4338866d9a75186580f76bc0e
ReenRex <governed.rex@gmail.com>
2026-07-20 21:44:38 +0100

    Update belgiumdata.js

--- Belgium/belgiumdata.js
+++ Belgium/belgiumdata.js
@@ -7,5 +7,23 @@ data.Europe = data.Europe || {};
 data.Europe.Belgium = {
   "18th Century": [],
   "19th Century": [],
-  "20th Century": []
+  "20th Century": [
+
+     {
+        title: "Belgium 1:25k (1939-43)",
+        url: "https://digicoll.lib.berkeley.edu/record/112516?ln=en&v=uv#?xywh=-1469%2C-217%2C11006%2C7453",
+        indexUrl: "https://digicoll.lib.berkeley.edu/record/105123?ln=en&v=uv#?xywh=-629%2C-434%2C14371%2C8160",
+     },
+     
+     {
+     title: "Belgium 1:50k (1943)",
+      url: "https://maps.lib.utexas.edu/maps/ams/belgium_50k/",
+     },
+
+     {
+        title: "Belgium 1:100k (1949)",
+        url: "https://maps.lib.utexas.edu/maps/ams/belgium_100k/",
+     },
+     
+        ]
 };
