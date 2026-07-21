17b1af44ae3e625c5fd78c2b8d0f89dc4fbd00fb
ReenRex <governed.rex@gmail.com>
2026-07-21 18:12:43 +0100

    Update netherlandsdata.js

--- Netherlands/netherlandsdata.js
+++ Netherlands/netherlandsdata.js
@@ -12,5 +12,22 @@ data.Europe.Netherlands = {
       url: "https://objects.library.uu.nl/reader/20.500.14918-352164?lan=en&page=1"
     }
   ],
-  "20th Century": []
+  "20th Century": [
+     {
+        title: "Netherlands 1:25k (1943-44)",
+        url: "https://www.loc.gov/resource/g6000m.gct00040/?st=gallery&c=160",
+        indexUrl: "https://nla.gov.au/nla.obj-752291496/view",
+     },
+     
+     {
+     title: "Netherlands 1:25k -- ADDITIONAL SHEET",
+        url: "https://archive.org/details/WCWdb_18189/Netherlands-R1.jpg"
+     },
+
+     {
+        title: "Eastern Netherlands 1:25k",
+        url: "https://www.loc.gov/resource/g6000m.gct00041/?st=gallery",
+     },
+        
+  ]
 };
