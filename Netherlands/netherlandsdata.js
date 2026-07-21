97a2c56e632958599af42574b99e9c2712f48bfc
ReenRex <governed.rex@gmail.com>
2026-07-21 17:36:58 +0100

    Update netherlandsdata.js

--- Netherlands/netherlandsdata.js
+++ Netherlands/netherlandsdata.js
@@ -1,1 +1,15 @@
-
+/* ============================================================
+   NETHERLANDS — edit this file to add or change the Netherland's maps.
+   See France/francedata.js for full instructions on adding
+   tutorials and notes to an entry.
+   ============================================================ */
+data.Europe = data.Europe || {};
+data.Europe.Netherlands = {
+  "18th Century": [],
+  "19th Century": [
+    {
+      title: "Netherlands 1:50k (1843)",
+      url: "https://objects.library.uu.nl/reader/20.500.14918-352164?lan=en&page=1"
+  ],
+  "20th Century": [],
+}
