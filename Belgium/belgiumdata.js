598f0ac18b73ddbeda15cddaa1051acc5573c96e
ReenRex <governed.rex@gmail.com>
2026-07-29 20:11:00 +0100

    Update belgiumdata.js

--- Belgium/belgiumdata.js
+++ Belgium/belgiumdata.js
@@ -15,7 +15,6 @@ data.Europe.Belgium = {
   ],
   "19th Century": [],
   "20th Century": [
-
      {
         title: "Belgium 1:25k (1939-43)",
         url: "https://digicoll.lib.berkeley.edu/record/112516?ln=en&v=uv#?xywh=-1469%2C-217%2C11006%2C7453",
@@ -25,6 +24,15 @@ data.Europe.Belgium = {
      {
      title: "Belgium 1:50k (1943)",
       url: "https://maps.lib.utexas.edu/maps/ams/belgium_50k/",
+     },
+
+     {
+        title: "Belgium 1:100k (1910-12)",
+        url: "https://nla.gov.au/nla.obj-3006190010/view",
+        indexUrl: "https://catalogue.nla.gov.au/catalog/6414975",
+        notes [
+     { body: "Unfortunately, these images are only available to be downloaded for a price or stitched together via multiple screenshots. Whilst this may sound like a lengthy process, a stitched map requires anywhere between 20 mins to an hour, depending on the sheet and zoom level.", }
+     ]
      },
 
         {
